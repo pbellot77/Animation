531ca0b46476ffc31d40b260574e402aef8d4b18
Patrick <patrickbellot77@gmail.com>
2017-12-14 13:09:50 -0600

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Patrick Bellot on 12/14/17.
+//  Copyright © 2017 Polestar Interactive LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
